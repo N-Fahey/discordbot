b44dcbfe14c6133d8d95de22e97f0ff3aa8a8650
Nick <nfahey@live.com.au>
2021-08-18 10:40:01 +1000

    Removed uid attribute & implemented !leave command

--- cogs/lobby.py
+++ cogs/lobby.py
@@ -1,7 +1,5 @@
 from discord.ext import commands,timers
-from discord import Embed
-from discord.ext.commands.errors import MissingPermissions
-import random
+from discord import Embed,Member
 
 #########################
 #       Extension       #
@@ -15,7 +13,6 @@ class Lobby:
         self.game_type = game_type
         self.game = None
         self.message = None
-        self.lobby_unique_id = '{:03}'.format(random.randrange(1, 10**3))
         self.timer = None
 
         # you can include stuff like lobby betting pot in here
@@ -38,7 +35,7 @@ class lobby(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
         self.bot.game_lobbies = []
-        self.bot.get_lobby_from_uid = self.get_lobby_from_uid
+        self.bot.get_lobby_from_owner = self.get_lobby_from_owner
         self.bot.get_member_lobby = self.get_member_lobby
         self.bot.lobby_end_game = self.lobby_end_game
 
@@ -49,9 +46,9 @@ class lobby(commands.Cog):
 
         return None
 
-    def get_lobby_from_uid(self,uid):
+    def get_lobby_from_owner(self,lobby_owner):
         for lobby in self.bot.game_lobbies:
-            if lobby.lobby_unique_id == uid:
+            if lobby.lobby_owner == lobby_owner:
                 return lobby
 
         return None
@@ -63,15 +60,14 @@ class lobby(commands.Cog):
 
     def get_lobby_embed_message(self,lobby, closed=False):
         embed = Embed(title=f"{lobby.lobby_owner.display_name} wants to play {self.bot.prettyGames[lobby.game_type]}")
-        player_list = ' '.join(i.name for i in lobby.lobby_players)
+        player_list = ', '.join(i.name for i in lobby.lobby_players)
         embed.set_thumbnail(url=lobby.lobby_owner.avatar_url)
-        embed.add_field(name='ID',value=lobby.lobby_unique_id)
         embed.add_field(name='Game',value=self.bot.prettyGames[lobby.game_type])
         embed.add_field(name='Owner',value=lobby.lobby_owner.mention)
         if not closed:
             embed.add_field(name='State',value="Waiting for players..." if not lobby.in_game else "In Game")
             embed.add_field(name='Players',value=player_list if len(player_list) > 0 else "None",inline=False)
-            embed.add_field(name='Instructions',value=f"If you want to join the lobby type !join {lobby.lobby_unique_id}",inline=False)
+            embed.add_field(name='Instructions',value=f"If you want to join the lobby type !join {lobby.lobby_owner.mention}",inline=False)
         else:
             embed.add_field(name='State',value="Closed")
             embed.add_field(name='Closed',value=f"Lobby is now closed.",inline=False)
@@ -86,7 +82,7 @@ class lobby(commands.Cog):
     #########################
     #join
     @commands.command(name="join",help="Join a currently open game lobby")
-    async def join(self,ctx, lobby_uid="default"):
+    async def join(self,ctx, lobby_owner:Member="default"):
         #Restrict lobby commands to game channel (Don't tell anyone that the game commands still work jeff)
         if ctx.channel.id != self.bot.game_channel_id:
             game_channel = self.bot.get_channel(self.bot.game_channel_id)
@@ -100,17 +96,17 @@ class lobby(commands.Cog):
             self.bot.dispatch("sendReply",ctx,"You're already in a lobby!")
             return
 
-        if not lobby_uid:
+        if not lobby_owner:
             self.bot.dispatch("sendReply",ctx,"lobby not found")
             return
         
         lobby_to_join = None
 
-        if lobby_uid == "default" and len(self.bot.game_lobbies) == 1:
+        if lobby_owner == "default" and len(self.bot.game_lobbies) == 1:
             lobby_to_join = self.bot.game_lobbies[0]
 
         else:
-            lobby_to_join = self.get_lobby_from_uid(lobby_uid)
+            lobby_to_join = self.get_lobby_from_owner(lobby_owner)
     
         
         if not lobby_to_join:
@@ -125,9 +121,7 @@ class lobby(commands.Cog):
         game_cog = self.bot.get_cog(lobby_to_join.game_type+"_game")
         if game_cog.lobby_capacity_check_join(lobby_to_join.lobby_players):
             lobby_to_join.lobby_players.append(ctx.author)
-            # gamePlayersPretty = ["`" + player.display_name + "`" for player in lobby_to_join.lobby_players]
             self.bot.dispatch("log",f"lobby: {ctx.author} joined {lobby_to_join.game_type} lobby")
-            # reply = f"`{ctx.author.display_name}` joined the {self.bot.prettyGames[lobby_to_join.game_type]} lobby. Currently waiting: {', '.join(gamePlayersPretty)}"
             await lobby_to_join.message.edit(embed=self.get_lobby_embed_message(lobby_to_join)) 
         else:
             reply = self.bot.get_cog(lobby_to_join.game_type+"_game").lobby_capacity_fail_message()
@@ -160,7 +154,7 @@ class lobby(commands.Cog):
             lobby.timer = timers.TimerManager(self.bot)
             lobby.timer.create_timer("lobbytimer",self.bot.lobbyTimeout,[lobby])
             self.bot.game_lobbies.append(lobby)
-            self.bot.dispatch("log",f"lobby: {ctx.author} created {match[0]} [{lobby.lobby_unique_id}] lobby.")
+            self.bot.dispatch("log",f"lobby: {ctx.author} created {match[0]} lobby.")
             lobby.message = await ctx.send(embed=self.get_lobby_embed_message(lobby)) 
             return
 
@@ -207,6 +201,29 @@ class lobby(commands.Cog):
         game_cog.on_game_start(ctx)
         return
     
+    #Leave lobby
+    @commands.command(name="leave",help="Leave the game lobby you're currently a member of")
+    async def leave_lobby(self,ctx):
+        #Restrict lobby commands to game channel (Don't tell anyone that the game commands still work jeff)
+        if ctx.channel.id != self.bot.game_channel_id:
+            game_channel = self.bot.get_channel(self.bot.game_channel_id)
+            if game_channel is not None:
+                self.bot.dispatch("sendReply",ctx,f"Games can only be played in the game channel: {game_channel.mention}")
+                return
+
+        member_lobby = self.get_member_lobby(ctx.author)
+        if not member_lobby:
+            self.bot.dispatch("sendReply",ctx,"You're not in a lobby")
+            return
+
+        if member_lobby.is_owner(ctx.author):
+            self.bot.dispatch("sendReply",ctx,"You're the lobby owner! You can only leave by cancelling the lobby with !cancel.")
+            return
+        
+        member_lobby.lobby_players.remove(ctx.author)
+        self.bot.dispatch("log",f"lobby: {ctx.author} left {member_lobby.game_type} lobby")
+        await member_lobby.message.edit(embed=self.get_lobby_embed_message(member_lobby))         
+
     #kill_lobby
     @commands.command(name="cancel",help="Close the currently open game lobby\nLobby will automatically time out after 5 minutes.")
     async def kill_lobby(self,ctx):
@@ -230,7 +247,7 @@ class lobby(commands.Cog):
 
         await member_lobby.message.edit(embed=self.get_lobby_embed_message(member_lobby,closed=True)) 
         self.bot.game_lobbies.remove(member_lobby)
-        reply = f"{self.bot.prettyGames[member_lobby.game_type]} [{member_lobby.lobby_unique_id}] lobby closed."
+        reply = f"{member_lobby.lobby_owner.mention}'s {self.bot.prettyGames[member_lobby.game_type]} lobby closed."
         self.bot.dispatch("log",f"lobby: {member_lobby.game_type} lobby killed by {ctx.author}.")
         self.bot.dispatch("sendReply",ctx,reply)
 
@@ -243,7 +260,7 @@ class lobby(commands.Cog):
         await member_lobby.message.edit(embed=self.get_lobby_embed_message(member_lobby,closed=True))
         self.bot.game_lobbies.remove(member_lobby)
         self.bot.dispatch("log",f"lobby: {member_lobby.game_type} lobby timed out.")
-        self.bot.dispatch("sendReply",member_lobby.message.channel,f"{self.bot.prettyGames[member_lobby.game_type]} [{member_lobby.lobby_unique_id}] lobby timed out.")
+        self.bot.dispatch("sendReply",member_lobby.message.channel,f"{member_lobby.lobby_owner.mention}'s {self.bot.prettyGames[member_lobby.game_type]} lobby timed out.")
         
 #########################
 #      FINAL SETUP      #
