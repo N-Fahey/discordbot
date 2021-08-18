d0ee917ce2502390b77a2eb1d12c913fa047c26d
Nick <nfahey@live.com.au>
2021-08-18 10:44:16 +1000

    Re-added settings default file

---
+++ settings.yaml.d
@@ -0,0 +1,12 @@
+prefixes: ["!"]
+game_channel_id: 876763826962464818
+lobbyTimeout: 300
+currencyCode: "߾"
+dolePayment: 100
+doleLimit: 1000
+doleTimeout: 86400
+
+prettyGames:
+  russianroulette: "Russian Roulette"
+  liarsdice: "Liar's Dice"
+  connect4: "Connect 4"
