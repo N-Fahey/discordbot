9e768ca7162b520eb3c1a6b59f13010185522472
N-Fahey <64514478+N-Fahey@users.noreply.github.com>
2021-09-29 08:55:11 +1000

    Fixed top10 error if nobody had any dosh

--- cogs/currency.py
+++ cogs/currency.py
@@ -60,14 +60,18 @@ class currency(commands.Cog):
         
         await ctx.send(f"`{ctx.author.display_name}`, your balance is {self.bot.currencyCode}{bal}")
     
+    #Top10
     @commands.command(name="top10", help="View the top 10 currency holders on the server.")
     async def test(self,ctx):
         sql_cog = self.bot.get_cog('sql')
         top10 = await sql_cog.queryTop10()
-        embed = Embed(title='The 1%')
-        embed.add_field(name="Name",value='\n'.join(top10.keys()))
-        embed.add_field(name="Bank",value='\n'.join(top10.values()))
-        await ctx.send(embed=embed)
+        if top10 == {}:
+            await ctx.send('Nobody has any money')
+        else:
+            embed = Embed(title='The 1%')
+            embed.add_field(name="Name",value='\n'.join(top10.keys()))
+            embed.add_field(name="Bank",value='\n'.join(top10.values()))
+            await ctx.send(embed=embed)
             
 def setup(bot):
     bot.add_cog(currency(bot))
